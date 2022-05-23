f316cb02f35d7dc690ada96876e806ff8aa5e309
RSILVERIOP <rsilverio.rafael@gmail.com>
2022-05-23 14:02:12 -0300

    Implementing UserDTOs and UserModel

---
+++ src/Api.Domain/Dtos/User/UserDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Api.Domain.Dtos.User
+{
+    public class UseDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(60)]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Format")]
+        [StringLength(100)]
+        public string Email { get; set; }
+    }
+}
